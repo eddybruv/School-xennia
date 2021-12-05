bdbd3886a5274724f9cf472a877b72b2c00a4b06
xennia316 <xennia316@gmail.com>
2021-12-05 05:07:44 +0100

    using the bubble sort method to sort lists

---
+++ CEF 341/Assignments/Sorting Algorithms/Bubble_sort.c
@@ -0,0 +1,49 @@
+/**
+ * @file Buble_sort.c
+ * @author xennia316/ Amehmbo Ngewung Sonia
+ * @brief sorting algorithms with the buble sort method.
+ * @version 0.1
+ * @date 2021-12-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+void swap( int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b =  temp;
+}
+// This function permutes the elements a and 
+
+void bubbleSort (int arr[], int p)
+{
+    int i;
+    int j;
+        for( i = 0; i < p-1; i++)
+        for( j = 0; j < p-i-1; j++)
+            if( arr[j]> arr[j + 1])
+            swap( &arr[j], &arr[j+1]);
+}
+// This is the bubble sort function that swaps adjacent values when wrongly placed.
+void output(int arr[], int size)
+{
+    int i;
+    for( i = 0; i < size; i++)
+    printf(" %d ", arr[i]);
+    printf("\n\n");
+}
+
+int main(void)
+{
+    int arr[] = {100, -34, 16, 5, 90, 1000, 100, 3, 2, 316};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, size);
+    printf("The sorted array is as follows: ");
+    output(arr, size);
+
+    return 0;
+
+}
