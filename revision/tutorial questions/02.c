492a93a8ad8a3d300cd9387e669317a2718bfd79
xennia316 <xennia316@gmail.com>
2022-01-19 07:32:54 +0100

    base and exponential function

---
+++ revision/tutorial questions/02.c
@@ -0,0 +1,32 @@
+/**
+ * @file 02.c
+ * @author Sonia Amehmbo @xennia316
+ * @brief recursive function that takes base and exponential and displays base^exp
+ * @version 0.1
+ * @date 2022-01-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int expo(int base, int exp)
+{
+    if (exp == 0)
+        return 1;
+    else
+        return base ^ expo(base, exp - 1);
+}
+
+int main(void)
+{
+    int base, exp;
+
+    printf("Enter exponential and base below: ");
+    scanf("%d %d", &base, &exp);
+
+    printf("%d %d \n", base, expo(base, exp));
+    return 0;
+}
