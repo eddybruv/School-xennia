7031c4cd1d33841ee592009f5ce1a8517602f884
xennia316 <xennia316@gmail.com>
2022-04-28 21:49:53 +0100

    javascript school course

---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+console.log("Hello................");
+alert("Hello there .......................");
+document.write("Hello world!");
+
+// Prompting
+let names =  prompt("Enter your name");
+alert("Your name is: " + names);
