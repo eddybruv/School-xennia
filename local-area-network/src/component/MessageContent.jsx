4ab02f80b90a38696911e5ce4a5e2b7b5d87dd17
eddybruv <edwinajong@gmail.com>
2022-05-04 09:12:41 +0100

    arranged sth

--- local-area-network/src/component/MessageContent.jsx
+++ local-area-network/src/component/MessageContent.jsx
@@ -8,19 +8,31 @@ const cipher = require("../cipher");
 const MessageContent = () => {
   let { content } = useContext(MessageContext);
   const [deMessage, setDeMessage] = useState(content);
-  const [showDecrypted, setShowDecrypted] = useState(false)
+  const [showDecrypted, setShowDecrypted] = useState(false);
   // let content = '1234'
   const [key, setKey] = useState(null);
   const handleChange = (e) => {
-    setDeMessage(content)
+    setDeMessage(content);
     setKey(e.target.value);
-    setDeMessage(false)
+    setDeMessage(false);
+  };
+
+  const checkInput = (key) => {
+    if (key === "") {
+      alert("Enter key!");
+      return false;
+    }
+    return true;
   };
 
   const handleDecryption = () => {
-    setShowDecrypted(true);
-    setDeMessage(cipher.decrypt(content, key));
-  }
+    if(checkInput)
+      {
+        setShowDecrypted(true);
+        setDeMessage(cipher.decrypt(content, key));
+        setKey('')
+      }
+  };
 
   return (
     <section className={classes.body}>
