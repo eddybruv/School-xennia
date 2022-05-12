8fd979fccc4fd8156ae3338a50a1089a27458bf4
xennia316 <xennia316@gmail.com>
2022-05-12 17:10:39 +0100

    refactored doc

--- local-area-network/Backend/server.js
+++ local-area-network/Backend/server.js
@@ -1,33 +1,36 @@
 const cipher = require("../src/cipher");
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MessageModel = require('./Models/message');
+const MessageModel = require("./Models/message");
 
-const connection = mongoose.connect("mongodb+srv://Sonia:ZENNIA0709@cluster0.hcmtn.mongodb.net/LanProject?retryWrites=true&w=majority");
+const connection = mongoose.connect(
+  "mongodb+srv://Sonia:ZENNIA0709@cluster0.hcmtn.mongodb.net/LanProject?retryWrites=true&w=majority"
+);
 
-connection.catch(() => console.log('Connection unsuccessful'));
-connection.then(() => console.log('Connection successful'))
+connection.catch(() => console.log("Connection unsuccessful"));
+connection.then(() => console.log("Connection successful"));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.post('/', async (req, res) => {
-    const { name, message } = req.body;
-    const newMessage = await new MessageModel({name: name, message: cipher.encrypt(message, 7)});
-    newMessage.save();
-    res.json({message: 'message sent', data: newMessage});
+app.post("/", async (req, res) => {
+  const { name, message } = req.body;
+  const newMessage = await new MessageModel({
+    name: name,
+    message: cipher.encrypt(message, 5),
+  });
+  newMessage.save();
+  res.json({ message: "message sent", data: newMessage });
 });
 
-app.get('/', async (req, res) => {
-    const messages = await MessageModel.find({});
-    res.json(messages);
+app.get("/", async (req, res) => {
+  const messages = await MessageModel.find({});
+  res.json(messages);
 });
-
-
 
 const PORT = 5000;
 
