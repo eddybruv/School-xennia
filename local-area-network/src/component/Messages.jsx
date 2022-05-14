cb73b6ff103fef3485edf37f0cbe330593a47420
xennia316 <xennia316@gmail.com>
2022-05-14 22:13:26 +0100

    message panel modified

--- local-area-network/src/component/Messages.jsx
+++ local-area-network/src/component/Messages.jsx
@@ -7,7 +7,7 @@ const Messages = () => {
   const [messages, setMessages] = useState(null);
 
   const fetchServer = async () => {
-    const result = await axios.get("http://172.20.10.13:5000/");
+    const result = await axios.get("http://localhost:5000/");
     setMessages(result.data);
   };
 
