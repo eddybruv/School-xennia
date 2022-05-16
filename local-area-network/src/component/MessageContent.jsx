6108eb93080e5db311b9f277ddb3851788aff927
eddybruv <edwinajong@gmail.com>
2022-05-16 12:57:41 +0100

    Revert "arranged sth"
    This reverts commit 4ab02f80b90a38696911e5ce4a5e2b7b5d87dd17.

--- local-area-network/src/component/MessageContent.jsx
+++ local-area-network/src/component/MessageContent.jsx
@@ -8,31 +8,19 @@ const cipher = require("../cipher");
 const MessageContent = () => {
   let { content } = useContext(MessageContext);
   const [deMessage, setDeMessage] = useState(content);
-  const [showDecrypted, setShowDecrypted] = useState(false);
+  const [showDecrypted, setShowDecrypted] = useState(false)
   // let content = '1234'
   const [key, setKey] = useState(null);
   const handleChange = (e) => {
-    setDeMessage(content);
+    setDeMessage(content)
     setKey(e.target.value);
-    setDeMessage(false);
-  };
-
-  const checkInput = (key) => {
-    if (key === "") {
-      alert("Enter key!");
-      return false;
-    }
-    return true;
+    setDeMessage(false)
   };
 
   const handleDecryption = () => {
-    if(checkInput)
-      {
-        setShowDecrypted(true);
-        setDeMessage(cipher.decrypt(content, key));
-        setKey('')
-      }
-  };
+    setShowDecrypted(true);
+    setDeMessage(cipher.decrypt(content, key));
+  }
 
   return (
     <section className={classes.body}>
