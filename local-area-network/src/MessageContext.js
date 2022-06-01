a7c1795c1ed2fb68209c3ccc7ebd167a90710570
Blanco237 <asongrandy9@gmail.com>
2022-06-01 08:19:10 +0100

    Updated url value in provider

--- local-area-network/src/MessageContext.js
+++ local-area-network/src/MessageContext.js
@@ -5,7 +5,7 @@ const MessageContext = createContext();
 
 export const MessageProvider = ({ children }) => {
     const [content,setContent] = useState('Test content which serves no purpose currently');
-    const url = 'http://192.168.43.168:5000';
+    const url = 'http://192.168.43.169:5000';
 
     return (
         <MessageContext.Provider value={{content,setContent, url}}>
