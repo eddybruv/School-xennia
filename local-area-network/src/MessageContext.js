e434eb7731ac0ef6e4ef1777121a56adcc3eeeda
xennia316 <xennia316@gmail.com>
2022-06-06 12:59:15 +0100

    message context

--- local-area-network/src/MessageContext.js
+++ local-area-network/src/MessageContext.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { createContext, useState,  } from "react";
+import { createContext, useState } from "react";
 
 const MessageContext = createContext();
 
 export const MessageProvider = ({ children }) => {
-    const [content,setContent] = useState('Test content which serves no purpose currently');
-    const url = 'http://192.168.43.169:5000';
+  const [content, setContent] = useState(
+    "Test content which serves no purpose currently"
+  );
+  const url = "http://192.168.43.168:5000";
 
-    return (
-        <MessageContext.Provider value={{content,setContent, url}}>
-            {children}
-        </MessageContext.Provider>
-    )
-}
+  return (
+    <MessageContext.Provider value={{ content, setContent, url }}>
+      {children}
+    </MessageContext.Provider>
+  );
+};
 
 export default MessageContext;
